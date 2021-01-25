80ea33d7b2b53b771d189222de9135bdf8dc58f6
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-01-25 04:42:33 +0000

    Revert "dma-buf: fix race while reading the dma_buf in dmabuffs_dname"
    This reverts commit daee5bd65dfca23adf7364d445cc804da9103f61.

--- drivers/dma-buf/dma-buf.c
+++ drivers/dma-buf/dma-buf.c
@@ -41,7 +41,6 @@
 #include <linux/list_sort.h>
 #include <linux/hashtable.h>
 #include <linux/mount.h>
-#include <linux/dcache.h>
 
 #include <uapi/linux/dma-buf.h>
 #include <uapi/linux/magic.h>
@@ -70,34 +69,18 @@ struct dma_proc {
 
 static struct dma_buf_list db_list;
 
-static void dmabuf_dent_put(struct dma_buf *dmabuf)
-{
-	if (atomic_dec_and_test(&dmabuf->dent_count)) {
-		kfree(dmabuf->name);
-		kfree(dmabuf);
-	}
-}
-
-
 static char *dmabuffs_dname(struct dentry *dentry, char *buffer, int buflen)
 {
 	struct dma_buf *dmabuf;
 	char name[DMA_BUF_NAME_LEN];
 	size_t ret = 0;
 
-	spin_lock(&dentry->d_lock);
 	dmabuf = dentry->d_fsdata;
-	if (!dmabuf || !atomic_add_unless(&dmabuf->dent_count, 1, 0)) {
-		spin_unlock(&dentry->d_lock);
-		goto out;
-	}
-	spin_unlock(&dentry->d_lock);
 	mutex_lock(&dmabuf->lock);
 	if (dmabuf->name)
 		ret = strlcpy(name, dmabuf->name, DMA_BUF_NAME_LEN);
 	mutex_unlock(&dmabuf->lock);
-	dmabuf_dent_put(dmabuf);
-out:
+
 	return dynamic_dname(dentry, buffer, buflen, "/%s:%s",
 			     dentry->d_name.name, ret > 0 ? name : "");
 }
@@ -106,11 +89,11 @@ static void dma_buf_release(struct dentry *dentry)
 {
 	struct dma_buf *dmabuf;
 
+	if (!is_dma_buf_file(file))
+		return -EINVAL;
+
 	dmabuf = dentry->d_fsdata;
 
-	spin_lock(&dentry->d_lock);
-	dentry->d_fsdata = NULL;
-	spin_unlock(&dentry->d_lock);
 	BUG_ON(dmabuf->vmapping_counter);
 
 	/*
@@ -131,7 +114,9 @@ static void dma_buf_release(struct dentry *dentry)
 		reservation_object_fini(dmabuf->resv);
 
 	module_put(dmabuf->owner);
-	dmabuf_dent_put(dmabuf);
+	kfree(dmabuf->buf_name);
+	kfree(dmabuf);
+	return 0;
 }
 
 static int dma_buf_file_release(struct inode *inode, struct file *file)
@@ -146,7 +131,6 @@ static int dma_buf_file_release(struct inode *inode, struct file *file)
 	mutex_lock(&db_list.lock);
 	list_del(&dmabuf->list_node);
 	mutex_unlock(&db_list.lock);
-
 	return 0;
 }
 
@@ -637,7 +621,6 @@ struct dma_buf *dma_buf_export(const struct dma_buf_export_info *exp_info)
 	dmabuf->cb_excl.active = dmabuf->cb_shared.active = 0;
 	dmabuf->buf_name = bufname;
 	dmabuf->ktime = ktime_get();
-	atomic_set(&dmabuf->dent_count, 1);
 
 	if (!resv) {
 		resv = (struct reservation_object *)&dmabuf[1];
