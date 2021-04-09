ce6aa17337196ee237460db17472cc0a11f2adcb
freak07 <michalechner92@googlemail.com>
2021-04-09 07:53:31 +0200

    zram: adjust for new changes and fix compiling issues
    (cherry picked from commit 6c7a15e9bd59f32d591a72f7604e55a160e0c393)
    (cherry picked from commit b36866bd6404b48a6036461d80a833e753d8e7f2)

--- drivers/md/dm.c
+++ drivers/md/dm.c
@@ -1533,13 +1533,15 @@ static void __split_and_process_bio(struct mapped_device *md,
  */
 static blk_qc_t dm_make_request(struct request_queue *q, struct bio *bio)
 {
+	int  __maybe_unused rw = bio_data_dir(bio);
 	struct mapped_device *md = q->queuedata;
 	int srcu_idx;
 	struct dm_table *map;
 
 	map = dm_get_live_table(md, &srcu_idx);
 
-	generic_start_io_acct(q, bio_op(bio), bio_sectors(bio), &dm_disk(md)->part0);
+	generic_start_io_acct(q, bio_op(bio), bio_sectors(bio),
+			      &dm_disk(md)->part0);
 
 	/* if we're suspended, we have to queue this io for later */
 	if (unlikely(test_bit(DMF_BLOCK_IO_FOR_SUSPEND, &md->flags))) {
