b8dfd175cada5fa23dbfca37fa8487bdaacd9404
Anh Tran <anh.tran.3889@gmail.com>
2021-10-01 22:19:51 +0700

    Fix compiling issues

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ mkdir -p out
 export PATH=~/workspace/proton-clang/bin:$PATH
 export CROSS_COMPILE=aarch64-linux-gnu-
 
-make O=./out ARCH=arm64 vendor/dragon_alpha_defconfig
+make O=./out ARCH=arm64 dragon_alpha_defconfig
 make O=./out ARCH=arm64 REAL_CC=clang -j16
