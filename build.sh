84d6fe41f0a574e03e4fbd8956fefe5a6b337e3c
Anh Tran <anh.tran.3889@gmail.com>
2021-10-04 09:26:53 +0700

    Update build script

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ export CROSS_COMPILE=aarch64-linux-gnu-
 
 make O=./out ARCH=arm64 vendor/dragon_alpha_defconfig
 make O=./out ARCH=arm64 REAL_CC=clang -j16
+cp -f ./out/arch/arm64/boot/Image.gz-dtb ./release/Dragon/Image.gz-dtb
